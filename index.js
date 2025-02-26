e19057ec8280838b3ced5d7ba45e26bb241e124b
fff324 <chenyu123aa@gmail.com>
2025-02-26 16:21:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // 请求路径。请修改此路径，避免该 worker 所有人都能使用。
-const endpointPath = '/dns-query';
+const endpointPath = '/666';
 // 上游 DoH 地址。必需是域名，不能是 IP。Cloudflare 有限制。
 const upstream = 'https://dns.google/dns-query';
 
